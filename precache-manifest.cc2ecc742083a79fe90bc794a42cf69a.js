74857b5a47cc4d8a723537c19450148432fbc0d2
gkousk <tp4399@edu.teicrete.gr>
2021-07-17 22:56:19 +0300

    Initial commit

---
+++ precache-manifest.cc2ecc742083a79fe90bc794a42cf69a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "568599d79b651859c8ef",
+    "url": "/static/css/main.68adc512.chunk.css"
+  },
+  {
+    "revision": "568599d79b651859c8ef",
+    "url": "/static/js/main.e5073f0a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "81c2ee440ce4c4be40de",
+    "url": "/static/css/2.5138a355.chunk.css"
+  },
+  {
+    "revision": "81c2ee440ce4c4be40de",
+    "url": "/static/js/2.78415423.chunk.js"
+  },
+  {
+    "revision": "592dfbcc0c9fdff2a0742e979a47099e",
+    "url": "/index.html"
+  }
+];
